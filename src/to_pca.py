54dcde13ba56f848149af1b6c54e882dc37f36e6
Vladimir Iglovikov <iglovikov@gmail.com>
2015-03-29 17:09:17 -0700

    bugfix. Script that uses PCA to decrease number of features added.

--- src/to_pca.py
+++ src/to_pca.py
@@ -10,8 +10,10 @@ import pandas as pd
 import os
 
 N = 100
-
+print "reading train"
 train = pd.read_csv(os.path.join("..", "data", "train_squared.csv"))
+
+print "reading test"
 test = pd.read_csv(os.path.join("..", "data", "test_squared.csv"))
 
 target = train["target"]
@@ -19,7 +21,7 @@ train_id = train["id"]
 test_id = test["id"]
 
 train = train.drop(["id", "target"], 1)
-test = test.drop("id")
+test = test.drop("id", 1)
 
 pca = decomposition.PCA(n_components=N)
 print "fitting"
